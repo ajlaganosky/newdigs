04ced232d0981bfdd6b42fcfbe7eb7eb859f19e8
aj <aj@717studios.com>
2013-01-30 11:24:58 -0500

    change for operation with heroku in application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-  #heroku requires
-  config.assets.initialize_on_precompile = false
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -61,6 +59,8 @@ module Inventory
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
+  #heroku requires
+  config.assets.initialize_on_precompile = false
   end
 end
