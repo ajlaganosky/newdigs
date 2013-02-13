010bd2051ebdf5788a532702ad6f923a6db00571
aj <aj@717studios.com>
2013-02-13 16:09:06 -0500

    wishlist implemented and minor fixes with migration commented out for heroku 20130211192358

--- db/migrate/20130211192358_add_date_time_to_events.rb
+++ db/migrate/20130211192358_add_date_time_to_events.rb
@@ -1,6 +1,6 @@
 class AddDateTimeToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :start_time, :datetime
-    add_column :events, :end_time, :datetime
+#    add_column :events, :start_time, :datetime
+#    add_column :events, :end_time, :datetime
   end
 end
