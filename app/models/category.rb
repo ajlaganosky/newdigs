600deef57c83faaf9ca8c5eeed01d7936dcae7d2
Christopher Mowers <chris@717studios.com>
2013-02-18 10:43:02 -0500

    Added fields and checks for item values

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :wishlist
+  attr_accessible :name, :wishlist, :price_a, :price_b, :price_c
   has_many :products
+  
+  validates :price_a, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, 
+:numericality =>{:greater_than => 0}
+	validates :price_b, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, 
+:numericality =>{:greater_than => 0}
+	validates :price_c, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, 
+:numericality =>{:greater_than => 0}
 end
