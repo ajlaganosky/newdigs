049ddb6f1a0b790b1172a20e22d2211ecd9fc317
Christopher Mowers <chris@717studios.com>
2013-02-18 09:25:57 -0500

    Fix migrations

--- db/migrate/20130205184338_changedonor_idtointeger.rb
+++
@@ -1,7 +0,0 @@
-class ChangedonorIdtointeger < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20130211192157_remove_date_time_from_events.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDateTimeFromEvents < ActiveRecord::Migration
-  def up
-    remove_column :events, :end_time
-  end
-  
-  def down
-  end
-end
